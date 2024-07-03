707aa3e249e366bd6b69c13d04a5676054503a32
geenr <ekiukkim90@gmail.com>
2024-07-03 13:09:55 -0700

    puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# this script creates a file /tmp
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
